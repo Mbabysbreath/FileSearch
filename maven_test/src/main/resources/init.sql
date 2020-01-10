460d66bac3562b88e9d01903325ed790f6a82927
Mbabysbreath <1830817543@qq.com>
2020-01-10 20:55:21 +0800

    1.输入一串字符，输出对应拼音

---
+++ maven_test/src/main/resources/init.sql
@@ -0,0 +1,12 @@
+drop table if exits file_meta;
+
+create table if not exits file_metal(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name VARCHAR(50) NOT NULL,
+      path VARCHAR(1000) NOT NULL,
+      is_directory BOOLEAN NOT NULL,
+      pinyin VARCHAR(50) NOT NULL,
+      pinyin_first VARCHAR(50) NOT NULL,
+      size BIGINT NOT NULL,
+      last_modified TIMESTAMP NOT NULL
+);
